84fe8749a884f8f39d0552b1e2744ad033f4dbec
Your Name <you@example.com>
2020-06-23 17:57:40 +0530

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         background_color: '#000000',
         theme_color: '#000000',
         display: 'minimal-ui',
-      //  icon: 'src/images/yo.png', // This path is relative to the root of the site.
+       icon: 'src/images/yo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
