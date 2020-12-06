a3834522bd5ea50660106c9ca69a5ae9adbc6ccd
Amar <amargithub@gmail.com>
2020-12-07 00:41:35 +0530

    add

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,12 +41,29 @@ const Layout = ({ children, location }) => {
               { name: 'keywords', content: 'sample, something' },
               {
                 name: 'og:image',
-                content:    
-                'https://res.cloudinary.com/amarcloud/image/upload/v1588864033/portfolio/images/bg_compressed.jpg'
+                // content:    
+                // 'https://res.cloudinary.com/amarcloud/image/upload/v1588864033/portfolio/images/bg_compressed.jpg'
               }
             ]}
           >
-            <html lang="en" />
+            <html lang="en">
+            <head>
+    <link rel="stylesheet" href="../assets/asset/css/main.css" />
+  </head>
+  <body>
+    <div id="particles-js">
+      <div class="header">
+        <h2>
+        {content}
+        </h2>
+    </div>
+</div>   
+    <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script src="../assets/asset/js/sweet-scroll.min.js"></script>
+    <script src="../assets/asset/js/google-analytics.js"></script>
+    <script src="../assets/asset/js/main.js"></script>
+  </body>
+            </html>
           </Helmet>
           {content}
         </>
